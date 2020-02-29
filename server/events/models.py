7c62e5dc7bcc0ebd63fd3d7e93fe33f5120ebb6b
Nemanja Djuric <nemanja.djuric@sbgenomics.com>
2020-02-29 14:21:00 +0100

    Add basic Event model
     - Added model for event
     - Added viewset for creating, fetching list or single event, and deleting event
     - Added viewset for registering players to event

--- server/events/models.py
+++ server/events/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from players.models import Player
+
+
+class Event(models.Model):
+    date = models.DateTimeField()
+    location = models.CharField(max_length=32)
+    players = models.ManyToManyField(Player, blank=True)
---
+++ server/events/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework.serializers import ModelSerializer
+
+from events.models import Event
+from players.serializers import PlayerSerializer
+
+
+class EventSerializer(ModelSerializer):
+    players = PlayerSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Event
+        fields = ('date', 'location', 'players')
