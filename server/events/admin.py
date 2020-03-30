f497b9f69b552a53b1461678b65df8516efe7baa
Nemanja Djuric <nemanja.djuric@outlook.com>
2020-03-30 19:16:07 +0200

    Register models to admin page

--- server/events/admin.py
+++ server/events/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from player.models import Player
+from events.models import Event
 
 # Register your models here.
+admin.site.register(Player)
+admin.site.register(Event)
